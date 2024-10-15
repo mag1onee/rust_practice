2845d693946ea1f142fe3b11805bce84c8e7a4ac
mag1onee <artemartemov2509@gmail.com>
2024-10-15 20:22:45 +0300

    Update chapter3 done.rs

--- src/chapter3.rs
+++ src/chapter3.rs
@@ -1,6 +1,7 @@
 /// https://practice.course.rs/variables.html
-#[test]
-fn test31() {
+//1 exercise
+// Fix the error below with least amount of modification to the code
+fn main() {
     let x: i32 = 5; // Uninitialized but used, ERROR !
     let _y: i32; // Uninitialized but also unused, only a Warning !
 
@@ -8,13 +9,95 @@ fn test31() {
     println!("Success!");
 }
 
-#[test]
-fn test399() {
-    let x = 5;
-    let y = 10;
-
-    let real = x + y;
-    let expected = 15;
-
-    assert_eq!(real, expected);
+//2 exercise
+// Fill the blanks in the code to make it compile
+fn main() {
+    let mut x: i32 = 1;
+    x += 2; 
+    
+    assert_eq!(x, 3);
+    println!("Success!");
 }
+
+//3 exercise
+// Fix the error below with least amount of modification
+fn main() {
+    let x: i32 = 10;
+    let y: i32 = 5;
+    {
+        println!("The value of x is {} and value of y is {}", x, y);
+    }
+    println!("The value of x is {} and value of y is {}", x, y); 
+}
+
+//4 exercise
+// Fix the error with the use of define_x
+fn main() {
+    define_x();
+}
+
+fn define_x() {
+    let x: &str = "hello";
+    println!("{}, world", x); 
+}
+
+//5 exercise
+// Only modify `assert_eq!` to make the `println!` work(print `42` in terminal)
+fn main() {
+    let x: i32 = 5;
+    {
+        let x = 12;
+        assert_eq!(x, 12);
+    }
+
+    assert_eq!(x, 5);
+
+    let x: i32 = 42;
+    println!("{}", x); // Prints "42".
+}
+
+//6 exercise
+// Remove a line in the code to make it compile
+fn main() {
+    let mut x: i32 = 1;
+    x = 7;
+    // Shadowing and re-binding
+    x += 3;
+
+
+    let y = 4;
+    // Shadowing
+    let y = "I can also be bound to text!"; 
+
+    println!("Success!");
+}
+
+//7 exercise
+#[allow(unused_variables)]
+fn main() {
+    let x = 1; 
+}
+// Warning: unused variable: `x`
+
+//8 exercise
+// Fix the error below with least amount of modification
+fn main() {
+    let (mut x, y) = (1, 2);
+    x += 2;
+
+    assert_eq!(x, 3);
+    assert_eq!(y, 2);
+
+    println!("Success!");
+}
+
+//9 exercise
+fn main() {
+    let (x, y);
+    (x,..) = (3, 4);
+    [.., y] = [1, 2];
+    // Fill the blank to make the code work
+    assert_eq!([x,y], [3, 2]);
+
+    println!("Success!");
+} 
